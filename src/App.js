fc07427ac03489dc13125c57e6a216a2a1e7a09d
WodaMineralna <raknieborak65@gmail.com>
2025-05-26 20:57:53 +0200

    Using useEffect() with Redux - sending a HTTP PUT to a Firebase Database
    - Storing the cart status of the user

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
+import { useSelector } from "react-redux";
+import { useEffect } from "react";
+
 import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
 
 function App() {
+  const cart = useSelector((state) => state.cart);
+
+  useEffect(() => {
+    fetch("https://s21-advanced-redux-default-rtdb.firebaseio.com/cart.json", {
+      method: "PUT",
+      body: JSON.stringify(cart),
+    });
+  }, [cart]);
+
   return (
     <Layout>
       <Cart />
